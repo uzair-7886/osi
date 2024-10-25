85d5f171c32fc73820a3574355501ae8b4178094
Hammad Ahmed <bosshammad0@gmail.com>
2024-10-25 15:54:00 +0500

    Uncommenting page.js /about

--- app/about/page.js
+++ app/about/page.js
@@ -1,26 +1,23 @@
-// import React from 'react'
-// import HeroBanner from '../components/shared/HeroBanner'
-// import SummerBanner from "../components/shared/SummerBanner";
-// import Intro from "../components/About/Intro";
-// import OurTeam from "../components/About/OurTeam";
-// import MethodologySection from "../components/About/Methodology";
-// import Tutors from "../components/About/Tutors";
-// import Deans from "../components/About/Deans";
+import React from 'react'
+import HeroBanner from '../components/shared/HeroBanner'
+import SummerBanner from "../components/shared/SummerBanner";
+import Intro from "../components/About/Intro";
+import OurTeam from "../components/About/OurTeam";
+import MethodologySection from "../components/About/Methodology";
+import Tutors from "../components/About/Tutors";
+import Deans from "../components/About/Deans";
 
 function page() {
     return (
-        // <>
-        //     <HeroBanner text="About Us" />
-        //     <Intro />
-        //     <OurTeam />
-        //     <MethodologySection />
-        //     <Tutors />
-        //     <Deans />
-        //     <SummerBanner />
-        // </>
-        <div>
-            ABOUT US
-        </div>
+        <>
+            <HeroBanner text="About Us" />
+            <Intro />
+            <OurTeam />
+            <MethodologySection />
+            <Tutors />
+            <Deans />
+            <SummerBanner />
+        </>
     )
 }
 
