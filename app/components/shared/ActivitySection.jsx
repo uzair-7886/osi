25ce91de07325e4bd7c15dfa8c409bbdbb4cb1ca
Uzair Khan <jerry.khan7886@gmail.com>
2024-10-31 17:47:40 +0500

    fix layout of activities and facilities

--- app/components/shared/ActivitySection.jsx
+++ app/components/shared/ActivitySection.jsx
@@ -3,37 +3,37 @@ import { urlFor } from '@/sanity/lib/image';
 
 function ActivitySection({activities}) {
   return (
-    <div className="w-full">
+    <div className="w-full text-grey">
         {activities.map((activity, index) => (
           <div 
             key={index} 
             className={`w-full py-12 px-4 sm:px-6 lg:px-8 ${
-              index % 2 === 1 ? 'bg-[#12243E] bg-opacity-10' : 'bg-white'
+              index % 2 === 1 ? 'bg-[#12243E] bg-opacity-10 ' : 'bg-white'
             }`}
           >
-            <div className="max-w-7xl mx-auto">
+            <div className="max-w-6xl mx-auto">
               <div className={`flex flex-col gap-8 ${
                 index % 2 === 0 ? 'lg:flex-row' : 'lg:flex-row-reverse'
               } items-center`}>
-                <div className="w-full lg:w-1/2 space-y-4 px-4">
+                <div className="w-full lg:w-1/2 md:space-y-8 space-y-4 px-4">
                   {activity.subtitle && (
-                    <h3 className="text-orange uppercase tracking-wide font-semibold text-sm">
-                      {activity.subtitle}
+                    <h3 className="text-orange uppercase tracking-wide font-semibold text-sm md:text-[22px]">
+                      {activity.name}
                     </h3>
                   )}
-                  <h3 className="text-2xl sm:text-3xl font-bold">{activity.name}</h3>
-                  <p className="text-gray-600 leading-relaxed">
+                  <h3 className="text-2xl md:text-[40px] font-semibold text-black">{activity.subtitle}</h3>
+                  <p className={` leading-relaxed ${index%2===0?'text-grey':'text-black'}` }>
                     {activity.description}
                   </p>
                 </div>
                 
                 <div className="w-full lg:w-1/2 px-4">
                   {activity.image && (
-                    <div className="rounded-tr-[40px] overflow-hidden shadow-lg">
+                    <div className="rounded-tr-[40px] h-auto md:w-[562px] md:h-[312px] overflow-hidden shadow-lg">
                       <img
-                        src={urlFor(activity.image).width(600).url()}
+                        src={urlFor(activity.image).url()}
                         alt={activity.name}
-                        className="w-full h-[200px] sm:h-[400px] object-cover"
+                        className="w-full  object-cover"
                       />
                     </div>
                   )}
