be82aa5a1b39de4d1603c2a9813f474ff298143f
Uzair Khan <jerry.khan7886@gmail.com>
2024-11-11 15:03:35 +0500

    Add registration flow

---
+++ app/application/page.js
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import RegistrationFlow from '../components/registration/Application'
+
+function page() {
+  return (
+    <>
+        <RegistrationFlow/>
+    </>
+  )
+}
+
+export default page
