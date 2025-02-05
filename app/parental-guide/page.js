712d8849fcab608087e6fc025ff1ceb22e9ae58f
Uzair Khan <jerry.khan7886@gmail.com>
2025-02-05 12:15:26 +0500

    add parental guide page

---
+++ app/parental-guide/page.js
@@ -0,0 +1,14 @@
+import HeroBanner from "../components/shared/HeroBanner"
+import ParentalGuide from "../components/ParentalGuide"
+
+
+const page = () => {
+  return (
+    <>
+      <HeroBanner  text={"A Parents' Guide"}/>
+      <ParentalGuide />
+    </>
+  )
+}
+
+export default page
