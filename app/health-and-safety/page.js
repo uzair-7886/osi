2c93d8d0cad0cba4d0e366dbcaa6de7a703f60bd
Uzair Khan <jerry.khan7886@gmail.com>
2025-04-17 21:58:16 +0500

    fix health and safety

--- app/health-and-safety/page.js
+++ app/health-and-safety/page.js
@@ -2,14 +2,12 @@
 import HeroBanner from "../components/shared/HeroBanner";
 import SummerBanner from "../components/shared/SummerBanner";
 import HealthAndSafety from "../components/HealthAndSafety";
-import TermsAndConditions from "../components/TermsAndConditions";
 
 const page = () => {
   return (
     <>
       <HeroBanner text="Health and Safety" />
-        {/* <HealthAndSafety /> */}
-        <TermsAndConditions/>
+        <HealthAndSafety />
       <SummerBanner/>
     </>
   )
